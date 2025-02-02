36ae386bf44f6f7dc504fb2c92baf5257b00894b
Shivam5278 <st527874@gmail.com>
2025-02-03 02:34:45 +0530

    Auth added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +10,7 @@ const app = new Hono<{
   };
 }>().basePath("/api/v1");
 
+app.use(cors());
 app.route("/user", userRouter);
 app.route("/blog", blogRouter);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
+import Signup from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
